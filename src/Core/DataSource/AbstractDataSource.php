1b208f7d32771d8896fa4fc704cd90d3328a8267
Yo! Symfony <yosymfony@gmail.com>
2015-03-25 19:46:38 +0000

    Added method getIncludes

--- src/Core/DataSource/AbstractDataSource.php
+++ src/Core/DataSource/AbstractDataSource.php
@@ -18,124 +18,126 @@ namespace Yosymfony\Spress\Core\DataSource;
  */
 abstract class AbstractDataSource
 {
-	protected $referenceCounter;
-	protected $params;
-	protected $isConfigured;
+    protected $referenceCounter;
+    protected $params;
+    protected $isConfigured;
 
-	/**
-	 * Constructor
-	 */
-	public function __construct(array $params)
-	{
-		$this->params = $params;
-		$this->referenceCounter = 0;
-		$this->isConfigured = false;
-	}
+    /**
+     * Constructor
+     */
+    public function __construct(array $params)
+    {
+        $this->params = $params;
+        $this->referenceCounter = 0;
+        $this->isConfigured = false;
+    }
 
-	/**
-	 * Returns the list of items
-	 * 
-	 * @return array
-	 */
-	abstract public function getItems();
+    /**
+     * Returns the list of items
+     *
+     * @return array
+     */
+    abstract public function getItems();
 
-	/**
-	 * Returns the list of items with type "layout".
-	 *
-	 * @return array
-	 */
-	abstract public function getLayouts();
+    /**
+     * Returns the list of items with type "layout".
+     *
+     * @return array
+     */
+    abstract public function getLayouts();
 
-	/**
-	 * Creates a new item or layout in the data source
-	 *
-	 * @param Item $item
-	 */
-	public function addItem(Item $item)
-	{
+    /**
+     * Returns the list of items with type "include".
+     *
+     * @return array
+     */
+    abstract public function getIncludes();
 
-	}
+    /**
+     * Creates a new item or layout in the data source
+     *
+     * @param Item $item
+     */
+    public function addItem(Item $item)
+    {
+    }
 
-	/**
-	 * Loads the data source
-	 */
-	public function load()
-	{
-		$this->addUse();
-		$this->process();
-		$this->removeUse();
-	}
+    /**
+     * Loads the data source
+     */
+    public function load()
+    {
+        $this->addUse();
+        $this->process();
+        $this->removeUse();
+    }
 
-	/**
-	 * Marks as used the data source.
-	 * This method increases the internal reference count.
-	 * When the internal reference count goes from 0 to 1 setUp method
-	 * is invoked.
-	 */
-	public function addUse()
-	{
-		if (false === $this->isConfigured) {
-			$this->configure();
-			$this->isConfigured = true;
-		}
+    /**
+     * Marks as used the data source.
+     * This method increases the internal reference count.
+     * When the internal reference count goes from 0 to 1 setUp method
+     * is invoked.
+     */
+    public function addUse()
+    {
+        if (false === $this->isConfigured) {
+            $this->configure();
+            $this->isConfigured = true;
+        }
 
-		if ($this->referenceCounter < 0) {
-			$this->referenceCounter = 0;
-		}
+        if ($this->referenceCounter < 0) {
+            $this->referenceCounter = 0;
+        }
 
-		if ($this->referenceCounter === 0) {
-			$this->setUp();
-		}
+        if ($this->referenceCounter === 0) {
+            $this->setUp();
+        }
 
-		$this->referenceCounter++;
-	}
+        $this->referenceCounter++;
+    }
 
-	/**
-	 * Marks as unused the data source.
-	 * This method decreases the internal reference count.
-	 * When the internal reference count is 0 tearDown method
-	 * is invoked.
-	 */
-	public function removeUse()
-	{
-		$this->referenceCounter--;
+    /**
+     * Marks as unused the data source.
+     * This method decreases the internal reference count.
+     * When the internal reference count is 0 tearDown method
+     * is invoked.
+     */
+    public function removeUse()
+    {
+        $this->referenceCounter--;
 
-		if ($this->referenceCounter === 0) {
-			$this->tearDown();
-		}
-	}
+        if ($this->referenceCounter === 0) {
+            $this->tearDown();
+        }
+    }
 
-	/**
-	 * Brings up the connections to the data.
-	 * e.g: this is the ideal place to connect to the database.
-	 */
-	public function setUp()
-	{
+    /**
+     * Brings up the connections to the data.
+     * e.g: this is the ideal place to connect to the database.
+     */
+    public function setUp()
+    {
+    }
 
-	}
+    /**
+     * Brings down the connections to the data.
+     * e.g: a database connection established in setUp should be closed in this method.
+     */
+    public function tearDown()
+    {
+    }
 
-	/**
-	 * Brings down the connections to the data.
-	 * e.g: a database connection established in setUp should be closed in this method.
-	 */
-	public function tearDown()
-	{
+    /**
+     * This method is used for setting up a site’s data source for the first time.
+     */
+    public function configure()
+    {
+    }
 
-	}
-
-	/**
-	 * This method is used for setting up a site’s data source for the first time.
-	 */
-	public function configure()
-	{
-
-	}
-
-	/**
-	 * All data source data manipulations and queries
-	 */
-	public function process()
-	{
-
-	}
+    /**
+     * All data source data manipulations and queries
+     */
+    public function process()
+    {
+    }
 }

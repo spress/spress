311b18158dd10c58dedf7623593a632cb04ac93b
Yo! Symfony <yosymfony@gmail.com>
2015-03-28 17:59:59 +0000

    Filesystem data source implemented

--- src/Core/DataSource/FilesystemDataSource.php
+++ src/Core/DataSource/FilesystemDataSource.php
@@ -12,74 +12,216 @@
 namespace Yosymfony\Spress\Core\DataSource;
 
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 /**
  * Data source for the filesystem.
  *
  * Params:
  *  - source_root	: the root directory for the main content.
- *  - layout_root	: the root directory for the layouts.
+ *  - layouts_root	: the root directory for the layouts.
  *  - includes_root	: the root directory for the includes.
  *  - posts_root	: the root directory for the posts.
+ *  - include 		: force to include files or directories.
+ *  - exclude		: force to exclude files or directories.
  *
  * @author Victor Puertas <vpgugr@gmail.com>
  */
 class FilesystemDataSource extends AbstractDataSource
 {
-	private $items;
-	private $layouts;
+    private $items;
+    private $layouts;
+    private $includes;
+    private $include;
+    private $exclude;
+    private $orgDir;
 
-	/**
-	 * @inheritDoc
-	 */
-	public function getItems()
-	{
-		return $this->items;
-	}
+    /**
+     * @inheritDoc
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function getLayouts()
-	{
-		return $this->layouts;
-	}
+    /**
+     * @inheritDoc
+     */
+    public function getLayouts()
+    {
+        return $this->layouts;
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function configure()
-	{
-		$this->items = [];
-		$this->layouts = [];
-	}
+    /**
+     * @inheritDoc
+     */
+    public function getIncludes()
+    {
+        return $this->includes;
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function process()
-	{
-		$this->processPosts();
-	}
+    /**
+     * @inheritDoc
+     */
+    public function configure()
+    {
+        $this->items = [];
+        $this->layouts = [];
+        $this->includes = [];
+        $this->include = [];
+        $this->exclude = [];
 
-	private function processPosts()
-	{
-		$finder = new Finder();
-		$finder->in($this->params['posts_root'])->files();
+        if (false === isset($this->params['source_root'])) {
+            throw new \RuntimeException('The data source expected param: "source_root".');
+        }
 
-		foreach ($finder as $file) {
-			$id = 'posts/'.$file->getRelativePathname();
-			$isBinary = $this->isBinary($file->getPathname());
+        if (false === is_string($this->params['source_root'])) {
+            throw new \RuntimeException('The data source expected a string at param: "source_root".');
+        }
 
-			$this->items[$id] = new Item($file->getContents(), $id, [], $isBinary);
-		}
-	}
+        if (true === isset($this->params['posts_root']) && false === is_string($this->params['posts_root'])) {
+            throw new \RuntimeException('The data source expected a string at param: "posts_root".');
+        }
 
-	private function isBinary($filename)
-	{
-		$finfo = finfo_open(FILEINFO_MIME_TYPE);
-		$mimeType = finfo_file($finfo, $filename);
+        if (true === isset($this->params['layouts_root']) && false === is_string($this->params['layouts_root'])) {
+            throw new \RuntimeException('The data source expected a string at param: "layouts_root".');
+        }
 
-		return 'text' !== substr($mimeType, 0, 4);
-	}
+        if (true === isset($this->params['includes_root']) && false === is_string($this->params['includes_root'])) {
+            throw new \RuntimeException('The data source expected a string at param: "includes_root".');
+        }
+
+        if (true === isset($this->params['include'])) {
+            if (true === is_array($this->params['include'])) {
+                $this->include = $this->params['include'];
+            } else {
+                throw new \RuntimeException('The data source expected an array at param: "include".');
+            }
+        }
+
+        if (true === isset($this->params['exclude'])) {
+            if (true === is_array($this->params['exclude'])) {
+                $this->exclude = $this->params['exclude'];
+            } else {
+                throw new \RuntimeException('The data source expected an array at param: "exclude".');
+            }
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process()
+    {
+        $this->processContentFiles();
+        $this->processLayoutFiles();
+        $this->processIncludeFiles();
+    }
+
+    public function setUp()
+    {
+        $this->orgDir = getcwd();
+        $this->setCurrentDir($this->params['source_root']);
+    }
+
+    public function tearDown()
+    {
+        $this->setCurrentDir($this->orgDir);
+    }
+
+    private function processContentFiles()
+    {
+        $includedFiles = [];
+
+        $finder = new Finder();
+        $finder->in($this->params['source_root'])
+            ->notPath('/^_/')
+            ->notPath('config.yml')
+            ->notPath('/config_.+\.yml/')
+            ->files();
+
+        if (isset($this->params['posts_root'])) {
+            $finder->in($this->params['posts_root']);
+        }
+
+        foreach ($this->include as $item) {
+            if (is_dir($item)) {
+                $finder->in($item);
+            } elseif (is_file($item)) {
+                $includedFiles[] = new SplFileInfo($item, '', pathinfo($item, PATHINFO_BASENAME));
+            }
+        }
+
+        $finder->append($includedFiles);
+
+        foreach ($this->exclude as $item) {
+            $finder->notPath($item);
+        }
+
+        $this->processItems($finder, Item::TYPE_ITEM);
+    }
+
+    private function processLayoutFiles()
+    {
+        if (false === isset($this->params['layouts_root'])) {
+            return;
+        }
+
+        $finder = new Finder();
+        $finder->in($this->params['layouts_root'])
+            ->files();
+
+        $this->processItems($finder, Item::TYPE_LAYOUT);
+    }
+
+    private function processIncludeFiles()
+    {
+        if (false === isset($this->params['includes_root'])) {
+            return;
+        }
+
+        $finder = new Finder();
+        $finder->in($this->params['includes_root'])
+            ->files();
+
+        $this->processItems($finder, Item::TYPE_INCLUDE);
+    }
+
+    private function processItems(Finder $finder, $type)
+    {
+        foreach ($finder as $file) {
+            $id = $file->getRelativePathname();
+            $isBinary = $this->isBinary($file->getPathname());
+
+            $item = new Item($file->getContents(), $id, [], $isBinary, $type);
+            $item->setPath($file->getRelativePathname());
+
+            switch ($type) {
+                case Item::TYPE_LAYOUT:
+                    $this->layouts[$id] = $item;
+                    break;
+                case Item::TYPE_INCLUDE:
+                    $this->includes[$id] = $item;
+                    break;
+                default:
+                   $this->items[$id] = $item;
+                    break;
+            }
+        }
+    }
+
+    private function isBinary($filename)
+    {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimeType = finfo_file($finfo, $filename);
+
+        return 'text' !== substr($mimeType, 0, 4);
+    }
+
+    private function setCurrentDir($path)
+    {
+        if (false === chdir($path)) {
+            throw new \InvalidArgumentException(sprintf('Error changing the current dir to "%s"', $path));
+        }
+    }
 }
--- src/Core/tests/DataSource/FilesystemDataSourceTest.php
+++ src/Core/tests/DataSource/FilesystemDataSourceTest.php
@@ -15,18 +15,190 @@ use Yosymfony\Spress\Core\DataSource\FilesystemDataSource;
 
 class FilesystemDataSourceTest extends \PHPUnit_Framework_TestCase
 {
-	public function testProcessItems()
-	{
-		$fsDataSource = new FilesystemDataSource([
-			'source_root' 	=> '',
-			'layout_root' 	=> '',
-			'includes_root' => '',
-			'posts_root' 	=> __dir__.'/../fixtures/project/_posts/',
-		]);
+    public function testProcessItems()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'layouts_root'    => __dir__.'/../fixtures/project/_layouts/',
+            'includes_root' => __dir__.'/../fixtures/project/_includes/',
+            'posts_root'    => __dir__.'/../fixtures/project/_posts/',
+        ]);
 
-		$fsDataSource->load();
+        $fsDataSource->load();
 
-		$this->assertTrue(is_array($fsDataSource->getItems()));
-		$this->assertTrue(is_array($fsDataSource->getLayouts()));
-	}
+        $items = $fsDataSource->getItems();
+        $layouts = $fsDataSource->getLayouts();
+        $includes = $fsDataSource->getIncludes();
+
+        $this->assertTrue(is_array($items));
+        $this->assertTrue(is_array($layouts));
+        $this->assertTrue(is_array($includes));
+
+        $this->assertCount(12, $items);
+        $this->assertCount(1, $layouts);
+        $this->assertCount(1, $includes);
+
+        $this->assertArrayHasKey('about/index.html', $items);
+        $this->assertArrayHasKey('about/me/index.html', $items);
+        $this->assertArrayHasKey('index.html', $items);
+        $this->assertArrayHasKey('LICENSE', $items);
+        $this->assertArrayHasKey('pages/index.html', $items);
+        $this->assertArrayHasKey('projects/index.md', $items);
+        $this->assertArrayHasKey('robots.txt', $items);
+        $this->assertArrayHasKey('sitemap.xml', $items);
+        $this->assertArrayHasKey('2013-08-12-post-example-1.md', $items);
+        $this->assertArrayHasKey('2013-08-12-post-example-2.mkd', $items);
+        $this->assertArrayHasKey('books/2013-08-11-best-book.md', $items);
+        $this->assertArrayHasKey('books/2013-09-19-new-book.md', $items);
+
+        $this->assertArrayHasKey('default.html', $layouts);
+
+        $this->assertArrayHasKey('test.html', $includes);
+
+        $item = $items['2013-08-12-post-example-1.md'];
+
+        $this->assertFalse($item->isBinary());
+
+        $layout = $layouts['default.html'];
+
+        $this->assertEquals('layout', $layout->getType());
+
+        $include = $includes['test.html'];
+
+        $this->assertEquals('include', $include->getType());
+    }
+
+    public function testIncludeFile()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'include'        => ['.htaccess'],
+        ]);
+        $fsDataSource->load();
+
+        $this->assertCount(9, $fsDataSource->getItems());
+    }
+
+    public function testIncludeFolder()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'include'        => ['../extra_pages'],
+        ]);
+        $fsDataSource->load();
+
+        $this->assertCount(10, $fsDataSource->getItems());
+    }
+
+    public function testExcludeFile()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'exclude'        => ['robots.txt'],
+        ]);
+        $fsDataSource->load();
+
+        $this->assertCount(7, $fsDataSource->getItems());
+    }
+
+    public function testExcludeFolder()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'exclude'        => ['about'],
+        ]);
+        $fsDataSource->load();
+
+        $this->assertCount(6, $fsDataSource->getItems());
+    }
+
+    public function testConfigOnlySourceRootParam()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+        ]);
+        $fsDataSource->load();
+
+        $this->assertCount(8, $fsDataSource->getItems());
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testConfigNoParams()
+    {
+        $fsDataSource = new FilesystemDataSource([]);
+        $fsDataSource->load();
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testBadParamSourceRoot()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => [],
+        ]);
+        $fsDataSource->load();
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testBadParamPostsRoot()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'posts_root'    => [],
+        ]);
+        $fsDataSource->load();
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testBadParamLayoutsRoot()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'layouts_root'    => [],
+        ]);
+        $fsDataSource->load();
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testBadParamIncludesRoot()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'includes_root'    => [],
+        ]);
+        $fsDataSource->load();
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testBadParamInclude()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'include'    => './',
+        ]);
+        $fsDataSource->load();
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testBadParamExclude()
+    {
+        $fsDataSource = new FilesystemDataSource([
+            'source_root'    => __dir__.'/../fixtures/project/',
+            'exclude'    => './',
+        ]);
+        $fsDataSource->load();
+    }
 }

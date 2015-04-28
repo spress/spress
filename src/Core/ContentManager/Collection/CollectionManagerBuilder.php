b2cd2915cedd9f7e216fc20ed4368469ecd106e0
Yo! Symfony <yosymfony@gmail.com>
2015-04-28 21:46:51 +0000

    Added Collection manager builder

---
+++ src/Core/ContentManager/Collection/CollectionManagerBuilder.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\Core\ContentManager\Collection;
+
+/**
+ * Build a Collection manager
+ *
+ * @author Victor Puertas <vpgugr@gmail.com>
+ */
+class CollectionManagerBuilder
+{
+    /**
+     * Build a collection manager with collections
+     * loaded from config array.
+     *
+     * Config array structure:
+     * .Array
+     * (
+     *   [collection_name_1] => Array
+     *        (
+     *            [attribute_1] => 'value'
+     *        )
+     *
+     *   [collection_name_2] => Array
+     *        (
+     *        )
+     * )
+     *
+     * @return \Yosymfony\Spress\Core\ContentManager\Collection\CollectionManager
+     */
+    public function buildFromConfigArray(array $config)
+    {
+        $cm = new CollectionManager();
+
+        foreach ($config as $collectionName => $attributes) {
+            $path = '_'.$collectionName;
+
+            if (is_array($attributes) === false) {
+                throw new \RuntimeException(sprintf('Expected array at the collection: "%s".', $collectionName));
+            }
+
+            $cm->add(new Collection($collectionName, $path, $attributes));
+        }
+
+        return $cm;
+    }
+}
---
+++ src/Core/tests/ContentManager/Collection/CollectionManagerBuilderTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\Core\tests\ContentManager\Collection;
+
+use Yosymfony\Spress\Core\ContentManager\Collection\CollectionManagerBuilder;
+
+class CollectionManagerBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBuildFromConfigArray()
+    {
+        $config = [
+            'events' => [
+                'output' => true,
+                'title'  => 'Events',
+            ],
+        ];
+
+        $builder = new CollectionManagerBuilder();
+        $cm = $builder->buildFromConfigArray($config);
+
+        $this->assertInstanceOf('\Yosymfony\Spress\Core\ContentManager\Collection\CollectionManager', $cm);
+
+        $collection = $cm->get('events');
+
+        $this->assertEquals('events', $collection->getName());
+        $this->assertEquals('_events', $collection->getPath());
+        $this->assertCount(2, $collection->getAttributes());
+        $this->assertArrayHasKey('title', $collection->getAttributes());
+        $this->assertArrayHasKey('output', $collection->getAttributes());
+    }
+
+    public function testBuildFromConfigArrayNoAttributes()
+    {
+        $config = [
+            'events' => [],
+        ];
+
+        $builder = new CollectionManagerBuilder();
+        $cm = $builder->buildFromConfigArray($config);
+
+        $this->assertInstanceOf('\Yosymfony\Spress\Core\ContentManager\Collection\CollectionManager', $cm);
+
+        $collection = $cm->get('events');
+
+        $this->assertEquals('events', $collection->getName());
+        $this->assertEquals('_events', $collection->getPath());
+        $this->assertCount(0, $collection->getAttributes());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testBuildFromConfigArrayWithBadData()
+    {
+        $config = [
+            'events' => true,
+        ];
+
+        $builder = new CollectionManagerBuilder();
+        $cm = $builder->buildFromConfigArray($config);
+    }
+}

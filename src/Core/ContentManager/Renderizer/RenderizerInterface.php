69a4ce4311d2540b886ef0f230ce67793314f323
Yo! Symfony <yosymfony@gmail.com>
2015-05-03 18:40:16 +0000

    Fixed namespace

--- src/Core/ContentManager/Renderizer/RenderizerInterface.php
+++ src/Core/ContentManager/Renderizer/RenderizerInterface.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Yosymfony\Spress\Core\ContentManager;
+namespace Yosymfony\Spress\Core\ContentManager\Renderizer;
 
 /**
  * Interface for rederizers

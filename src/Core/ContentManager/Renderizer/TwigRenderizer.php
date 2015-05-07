51c56deb9162cd74c531b6ba0e163cb9902d8ec5
Yo! Symfony <yosymfony@gmail.com>
2015-05-07 21:36:44 +0000

    Render a page implemented

--- src/Core/ContentManager/Renderizer/TwigRenderizer.php
+++ src/Core/ContentManager/Renderizer/TwigRenderizer.php
@@ -22,25 +22,56 @@ class TwigRenderizer implements RenderizerInterface
 {
     protected $twig;
     protected $arrayLoader;
+    protected $layoutExtension;
 
-    public function __construct(\Twig_Environment $twig, \Twig_Loader_Array $arrayLoader)
+    /**
+     * Construct
+     *
+     * @param \Twig_Environment  $twig
+     * @param \Twig_Loader_Array $arrayLoader
+     * @param array              $layoutExtension
+     */
+    public function __construct(\Twig_Environment $twig, \Twig_Loader_Array $arrayLoader, array $layoutExtension)
     {
         $this->twig = $twig;
         $this->arrayLoader = $arrayLoader;
+        $this->layoutExtension = $layoutExtension;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function addLayout($name, $content, array $attributes = [])
     {
         $fullname = $this->getLayoutNameWithNamespace($name);
+        $layout = $this->getLayoutAttribute($attributes, $name);
+
+        if ($layout) {
+            $fullLayout = $this->getLayoutWithExtension($layout, $name);
+
+            if (isset($attributes['page']) === false) {
+                $attributes['page'] = [];
+            }
+
+            $attributes['page']['content'] = $content;
+
+            $content = sprintf('{%% extends "%s" %%}%s', $fullLayout, $content);
+        }
 
         $this->arrayLoader->setTemplate($fullname, $content);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function addInclude($name, $content, array $attributes = [])
     {
         $this->arrayLoader->setTemplate($name, $content);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function renderBlocks($name, $content, array $attributes)
     {
         $this->arrayLoader->setTemplate('@dynamic/content', $content);
@@ -55,21 +86,35 @@ class TwigRenderizer implements RenderizerInterface
      */
     public function renderPage($name, $content, array $attributes)
     {
-        
+        $layout = $this->getLayoutAttribute($attributes, $name);
+
+        if ($layout) {
+            $fullLayout = $this->getLayoutWithExtension($layout, $name);
+
+            if (isset($attributes['page']) === false) {
+                $attributes['page'] = [];
+            }
+
+            $attributes['page']['content'] = $content;
+
+            $content = sprintf('{%% extends "%s" %%}', $fullLayout);
+        }
+
+        return $this->renderBlocks($name, $content, $attributes);
     }
 
-    protected function getLayoutAttribute(array $attributes, $name)
+    protected function getLayoutAttribute(array $attributes, $contentName)
     {
         if (isset($attributes['layout']) === false) {
             return false;
         }
 
         if (is_string($attributes['layout']) === false) {
-            throw new AttributeValueException('Invalid value. Expected string.', 'layout', $name);
+            throw new AttributeValueException('Invalid value. Expected string.', 'layout', $contentName);
         }
 
         if (strlen($attributes['layout']) == 0) {
-            throw new AttributeValueException('Invalid value. Expected a non-empty string.', 'layout', $name);
+            throw new AttributeValueException('Invalid value. Expected a non-empty string.', 'layout', $contentName);
         }
 
         return $this->getLayoutNameWithNamespace($attributes['layout']);
@@ -78,5 +123,22 @@ class TwigRenderizer implements RenderizerInterface
     protected function getLayoutNameWithNamespace($name)
     {
         return '@layout/'.$name;
+    }
+
+    protected function getLayoutWithExtension($layoutName, $contentName)
+    {
+        foreach ($this->layoutExtension as $extension) {
+            $fullname = $layoutName.'.'.$extension;
+
+            if ($this->arrayLoader->exists($fullname)) {
+                return $fullname;
+            }
+        }
+
+        if ($this->arrayLoader->exists($layoutName) === true) {
+            return $layoutName;
+        }
+
+        throw new AttributeValueException(sprintf('Layout "%s" not found.', $name), 'layout', $contentName);
     }
 }
--- src/Core/tests/ContentManager/Renderizer/TwigRenderizerTest.php
+++ src/Core/tests/ContentManager/Renderizer/TwigRenderizerTest.php
@@ -43,11 +43,30 @@ class TwigRenderizerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("What's it?: This is a message.", $rendered);
     }
 
+    public function testRenderTemplateWithLayout()
+    {
+        $renderizer = $this->getRenderizer();
+        $renderizer->addLayout('default.twig', '<h1>Hi</h1>{% block content %}{{ page.content }}{% endblock %}');
+        $rendered = $renderizer->renderPage('index.html', 'Yo! Symfony', ['layout' => 'default']);
+
+        $this->assertEquals('<h1>Hi</h1>Yo! Symfony', $rendered);
+    }
+
+    public function testRenderTemplateWithStackLayouts()
+    {
+        $renderizer = $this->getRenderizer();
+        $renderizer->addLayout('default', '<html></body>{% block page %}{{ page.content }}{% endblock %}</body></html>');
+        $renderizer->addLayout('page', '{% block page %}<h1>Hi</h1>{{ page.content }}{% endblock %}', ['layout' => 'default']);
+        $rendered = $renderizer->renderPage('index.html', 'Yo! Symfony', ['layout' => 'page']);
+
+        $this->assertEquals('<html></body><h1>Hi</h1>Yo! Symfony</body></html>', $rendered);
+    }
+
     private function getRenderizer()
     {
         $twigLoader = new \Twig_Loader_Array([]);
         $twig = new \Twig_Environment($twigLoader, ['autoescape' => false]);
 
-        return new TwigRenderizer($twig, $twigLoader);
+        return new TwigRenderizer($twig, $twigLoader, ['twig']);
     }
 }

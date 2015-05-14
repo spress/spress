d27cb6a030906bf7b5974999cce5b87dd6c8977a
Yo! Symfony <yosymfony@gmail.com>
2015-05-14 19:23:17 +0000

    Added interface for generators

---
+++ src/Core/ContentManager/Generator/GeneratorInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\Core\ContentManager\Generator;
+
+use Yosymfony\Spress\Core\DataSource\ItemInterface;
+
+/**
+ * Iterface for generators.
+ *
+ * Generators are used for generating new items of content.
+ * e.g: tags or categories for posts.
+ *
+ * @author Victor Puertas <vpgugr@gmail.com>
+ */
+interface GeneratorInterface
+{
+    /**
+     * Generate items.
+     *
+     * @return array List of Yosymfony\Spress\Core\DataSource\ItemInterface
+     */
+    public function generateItems(ItemInterface $templateItem, array $attributes);
+}

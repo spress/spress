e9f290d623c6bcfaafa444d554a090ccc7118e8b
Yo! Symfony <yosymfony@gmail.com>
2015-05-18 19:20:17 +0000

    A wrapper for working with arrays

---
+++ src/Core/Support/ArrayWrapper.php
@@ -0,0 +1,158 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\Core\Support;
+
+/**
+  * Array box is a wrapper for working with arrays.
+  *
+  * Based on https://github.com/laravel/framework/blob/5.0/src/Illuminate/Support/Arr.php
+  *
+  * @author Victor Puertas <vpgugr@gmail.com>
+  */
+class ArrayWrapper
+{
+    protected $array;
+
+    public function __construct(array $array)
+    {
+        $this->array = $array;
+    }
+
+    /**
+     * Add an element using "dot" notation if doesn't exists
+     *
+     * @param $key
+     * @param $value
+     *
+     * @return array
+     */
+    public function add($key, $value)
+    {
+        if ($this->has($key) === false) {
+            $this->set($key, $value);
+        }
+
+        return $this->array;
+    }
+
+    /**
+     * Get a value from a deeply nested array using "dot" notation
+     *
+     * e.g: $a->get('site.data')
+     *
+     * @param string $key
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        $array = $this->array;
+
+        if (isset($array[$key])) {
+            return $array[$key];
+        }
+
+        foreach (explode('.', $key) as $step) {
+            if (is_array($array) === false || array_key_exists($step, $array) === false) {
+                return $default;
+            }
+
+            $array = $array[$step];
+        }
+
+        return $array;
+    }
+
+    /**
+     * Check if an item exists in using "dot" notation
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function has($key)
+    {
+        $array = $this->array;
+
+        if (empty($array) || is_null($key)) {
+            return false;
+        }
+
+        if (array_key_exists($key, $array)) {
+            return true;
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (is_array($array) === false || array_key_exists($segment, $array) === false) {
+                return false;
+            }
+
+            $array = $array[$segment];
+        }
+
+        return true;
+    }
+
+    /**
+     * Set an item using "dot" notation
+     *
+     * @param string $key
+     * @param mixed  $value
+     *
+     * @return array
+     */
+    public function set($key, $value)
+    {
+        $array = &$this->array;
+
+        if (is_null($key)) {
+            return $array;
+        }
+
+        $keys = explode('.', $key);
+
+        while (count($keys) > 1) {
+            $key = array_shift($keys);
+
+            if (isset($array[$key]) === false || is_array($array[$key]) === false) {
+                $array[$key] = [];
+            }
+
+            $array = & $array[$key];
+        }
+
+        $array[array_shift($keys)] = $value;
+
+        return $this->array;
+    }
+
+    /**
+     * Filter using the given callback
+     *
+     * @param callable $callback
+     *
+     * @return array
+     */
+    public function where(callable $callback)
+    {
+        $filtered = [];
+
+        foreach ($this->array as $key => $value) {
+            if (call_user_func($callback, $key, $value)) {
+                $filtered[$key] = $value;
+            }
+        }
+
+        return $filtered;
+    }
+}
---
+++ src/Core/tests/Support/ArrayWrapperTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\Core\tests\Support;
+
+use Yosymfony\Spress\Core\Support\ArrayWrapper;
+
+class ArrayWrapperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        $a = new ArrayWrapper([]);
+        $a->add('name', 'Yo! Symfony');
+        $a->add('name', 'Yo! Symfony 2');
+        $a->add('title', 'Hi');
+
+        $this->assertEquals('Yo! Symfony', $a->get('name'));
+        $this->assertEquals('Hi', $a->get('title'));
+    }
+
+    public function testGet()
+    {
+        $a = new ArrayWrapper([
+            'site' => [
+                'name' => 'Yo! Symfony',
+            ],
+        ]);
+
+        $this->assertTrue(is_array($a->get('site')));
+        $this->assertEquals('Yo! Symfony', $a->get('site.name'));
+        $this->assertEquals('Default value', $a->get('site.not-exists', 'Default value'));
+        $this->assertEquals('Default value', $a->get('', 'Default value'));
+        $this->assertEquals('Default value', $a->get(null, 'Default value'));
+        $this->assertNull($a->get(''));
+        $this->assertNull($a->get(null));
+    }
+
+    public function testHas()
+    {
+        $a = new ArrayWrapper([]);
+        $a->set('site.name', 'Yo! Symfony');
+
+        $this->assertTrue($a->has('site.name'));
+        $this->assertFalse($a->has('site.notExists'));
+    }
+
+    public function testSet()
+    {
+        $a = new ArrayWrapper([]);
+        $a->set('site.name', 'Yo! Symfony');
+
+        $this->assertEquals('Yo! Symfony', $a->get('site.name'));
+    }
+
+    public function testWhere()
+    {
+        $data = [];
+        $a = new ArrayWrapper(['value1' => 1, 'value2' => 2]);
+
+        $filtered = $a->where(function ($key, $value) {
+            return $value > 1;
+        });
+
+        $this->assertCount(1, $filtered);
+    }
+}

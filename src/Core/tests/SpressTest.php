571654507b56a03de62792f82c05e82d793ca7f6
Yo! Symfony <yosymfony@gmail.com>
2015-07-15 16:54:43 +0000

    Changed default data writer by memory datawriter

--- src/Core/tests/SpressTest.php
+++ src/Core/tests/SpressTest.php
@@ -11,14 +11,28 @@
 
 namespace Yosymfony\Spress\Core\tests;
 
+use Yosymfony\Spress\Core\DataWriter\MemoryDataWriter;
 use Yosymfony\Spress\Core\Spress;
 
 class SpressTest extends \PHPUnit_Framework_TestCase
 {
     public function testParseSite()
     {
+        $dw = new MemoryDataWriter();
+
         $spress = new Spress();
         $spress['spress.config.site_dir'] = __dir__.'/fixtures/project';
+        $spress['spress.dataWriter'] = $dw;
         $spress->parse();
+
+        $this->assertCount(14, $dw->getItems());
+
+        $this->assertTrue($dw->hasItem('about/index.html'));
+        $this->assertTrue($dw->hasItem('pages/index.html'));
+        $this->assertTrue($dw->hasItem('pages/page2/index.html'));
+
+        $this->assertContains('<!DOCTYPE HTML>', $dw->getItem('about/index.html')->getContent());
+        $this->assertContains('<!DOCTYPE HTML>', $dw->getItem('pages/index.html')->getContent());
+        $this->assertContains('<!DOCTYPE HTML>', $dw->getItem('pages/page2/index.html')->getContent());
     }
 }

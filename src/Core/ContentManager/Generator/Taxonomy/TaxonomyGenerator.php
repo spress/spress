68cc80d846744965ef6b4dc3f88c03ceee51499d
Yo! Symfony <yosymfony@gmail.com>
2015-09-23 18:45:03 +0000

    Added taxonomy generator. Deals with #37

---
+++ src/Core/ContentManager/Generator/Taxonomy/TaxonomyGenerator.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\Core\ContentManager\Generator\Taxonomy;
+
+use Yosymfony\Spress\Core\ContentManager\Generator\GeneratorInterface;
+use Yosymfony\Spress\Core\ContentManager\Generator\Pagination\PaginationGenerator;
+use Yosymfony\Spress\Core\DataSource\ItemInterface;
+use Yosymfony\Spress\Core\Support\ArrayWrapper;
+use Yosymfony\Spress\Core\Support\AttributesResolver;
+
+/**
+ * Taxonomy generator.
+ *
+ * How to configure? (frontmatter of the template page):
+ *
+ * ---
+ * layout: default
+ * generator: taxonomy
+ * max_page: 5
+ * taxonomy_attribute: 'categories'
+ * permalink: '/:name'
+ * pagination_permalink: '/page:num'
+ * ---
+ *
+ * @author Victor Puertas <vpgugr@gmail.com>
+ */
+class TaxonomyGenerator implements GeneratorInterface
+{
+    /**
+     * @inheritDoc
+     *
+     * @throws Yosymfony\Spress\Core\Exception\AttributeValueException if bad attribute value.
+     */
+    public function generateItems(ItemInterface $templateItem, array $collections)
+    {
+        $result = [];
+        $taxonomyCollection = [];
+        $templateAttributes = $templateItem->getAttributes();
+        $options = $this->getAttributeResolver($templateItem);
+        $taxonomyAttribute = $options['taxonomy_attribute'];
+        $permalink = $options['permalink'];
+        $templateAttributes['permalink'] = $options['pagination_permalink'];
+        $templatePath = dirname($templateItem->getPath(ItemInterface::SNAPSHOT_PATH_RELATIVE));
+
+        $items = (new ArrayWrapper($collections))->flatten();
+
+        foreach ($items as $item) {
+            $attributes = $item->getAttributes();
+
+            if (isset($attributes[$taxonomyAttribute]) === false) {
+                continue;
+            }
+
+            $taxa = (array) $attributes[$taxonomyAttribute];
+
+            foreach ($taxa as $taxon) {
+                $taxonomyCollection[$taxon][] = $item;
+            }
+        }
+
+        foreach ($taxonomyCollection as $taxon => $items) {
+            $templateAttributes['provider'] = 'site.'.$taxon;
+            $templateItem->setAttributes($templateAttributes);
+            $taxonPath = $this->getTaxonRelativePath($templatePath, $permalink, $taxon);
+            $templateItem->setPath($taxonPath, ItemInterface::SNAPSHOT_PATH_RELATIVE);
+
+            $paginationGenerator = new PaginationGenerator();
+            $itemsGenerated = $paginationGenerator->generateItems($templateItem, [$taxon => $items]);
+
+            $result = array_merge($result, $itemsGenerated);
+        }
+
+        return $result;
+    }
+
+    protected function getTaxonRelativePath($basePath, $permalinkTemplate, $taxon)
+    {
+        $result = $basePath;
+        $result .= '/'.str_replace(':name', $taxon, $permalinkTemplate);
+
+        return ltrim(preg_replace('/\/\/+/', '/', $result), '/');
+    }
+
+    protected function getAttributeResolver(ItemInterface $templateItem)
+    {
+        $resolver = new AttributesResolver();
+        $resolver->setDefault('taxonomy_attribute', 'categories', 'string')
+            ->setDefault('permalink', '/:name/page:num')
+            ->setDefault('pagination_permalink', '/page:num', 'string');
+
+        $attributes = $templateItem->getAttributes();
+
+        return $resolver->resolve($attributes);
+    }
+}
---
+++ src/Core/Tests/ContentManager/Generator/Taxonomy/TaxonomyGeneratorTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\Core\Tests\ContentManager\Generator\Taxonomy;
+
+use Yosymfony\Spress\Core\DataSource\Item;
+use Yosymfony\Spress\Core\ContentManager\Generator\Taxonomy\TaxonomyGenerator;
+
+class TaxonomyGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    protected $taxonomy;
+
+    public function setUp()
+    {
+        $this->taxonomy = new TaxonomyGenerator();
+    }
+
+    public function testPaginateWithDefaulValues()
+    {
+        $post1 = new Item('Post 1', 'posts/2015-05-26-new-release', ['categories' => ['news', 'releases']]);
+        $post2 = new Item('Post 2', 'posts/2015-05-26-new-feature', ['categories' => ['news']]);
+
+        $collections = [
+            'posts' => [$post1, $post2],
+        ];
+
+        $templateItem = new Item('Categories content', 'categories/index.html', ['max_page' => 1]);
+        $templateItem->setPath('categories/index.html', Item::SNAPSHOT_PATH_RELATIVE);
+
+        $catetoriesItems = $this->taxonomy->generateItems($templateItem, $collections);
+
+        $this->assertTrue(is_array($catetoriesItems));
+        $this->assertCount(3, $catetoriesItems);
+        $this->assertContainsOnly('\Yosymfony\Spress\Core\DataSource\ItemInterface', $catetoriesItems);
+
+        $item = $catetoriesItems[0];
+
+        $this->assertEquals('categories/news/index.html', $item->getPath(Item::SNAPSHOT_PATH_RELATIVE));
+        $this->assertEquals('/categories/news', $item->getPath(Item::SNAPSHOT_PATH_PERMALINK));
+        $this->assertEquals('Categories content', $item->getContent());
+
+        $item = $catetoriesItems[1];
+
+        $this->assertEquals('categories/news/page2/index.html', $item->getPath(Item::SNAPSHOT_PATH_RELATIVE));
+        $this->assertEquals('/categories/news/page2', $item->getPath(Item::SNAPSHOT_PATH_PERMALINK));
+        $this->assertEquals('Categories content', $item->getContent());
+
+        $item = $catetoriesItems[2];
+
+        $this->assertEquals('categories/releases/index.html', $item->getPath(Item::SNAPSHOT_PATH_RELATIVE));
+        $this->assertEquals('/categories/releases', $item->getPath(Item::SNAPSHOT_PATH_PERMALINK));
+        $this->assertEquals('Categories content', $item->getContent());
+    }
+}

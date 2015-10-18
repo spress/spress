9b51c4767cf040973e88f779835e3df7512ca500
Yo! Symfony <yosymfony@gmail.com>
2015-10-18 15:42:39 +0000

    Changed version to 2.0.0-rc

--- src/Core/Spress.php
+++ src/Core/Spress.php
@@ -50,12 +50,12 @@ use Yosymfony\Spress\Core\Plugin\PluginManagerBuilder;
  */
 class Spress extends Container
 {
-    const VERSION = '2.0.0-beta';
+    const VERSION = '2.0.0-rc';
     const VERSION_ID = '20000';
     const MAJOR_VERSION = '2';
     const MINOR_VERSION = '0';
     const RELEASE_VERSION = '0';
-    const EXTRA_VERSION = 'beta';
+    const EXTRA_VERSION = 'rc';
 
     public function __construct()
     {

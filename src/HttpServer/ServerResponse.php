2858a761e160dcc88e6ccf642da085f9d40f5312
Yo! Symfony <yosymfony@gmail.com>
2015-11-30 19:58:39 +0000

    Added documentation

--- src/HttpServer/ServerResponse.php
+++ src/HttpServer/ServerResponse.php
@@ -22,6 +22,13 @@ class ServerResponse
     private $statusCode;
     private $contentType;
 
+    /**
+     * Constructor.
+     * 
+     * @param string $content     Content of the response.
+     * @param int    $statusCode  The status code.
+     * @param string $contentType The MIME type.
+     */
     public function __construct($content, $statusCode = 200, $contentType = 'text/html')
     {
         $this->content = $content;
@@ -29,31 +36,61 @@ class ServerResponse
         $this->contentType = $contentType;
     }
 
+    /**
+     * Set the content of the response.
+     * 
+     * @param string $content
+     */
     public function setContent($content)
     {
         $this->content = $content;
     }
 
+    /**
+     * Get the content of the response.
+     * 
+     * @return string
+     */
     public function getContent()
     {
         return $this->content;
     }
 
+    /**
+     * Set the status code. e.g: 200 for ok response.
+     * 
+     * @param int $code
+     */
     public function setStatusCode($code)
     {
         $this->statusCode = $code;
     }
 
+    /**
+     * Get the status code.
+     * 
+     * @return int
+     */
     public function getStatusCode()
     {
         return $this->statusCode;
     }
 
+    /**
+     * Set the MIME type. e.g: 'text/html'.
+     * 
+     * @param string $type
+     */
     public function setContentType($type)
     {
         $this->contentType = $type;
     }
 
+    /**
+     * Get the MIME type.
+     * 
+     * @return string
+     */
     public function getContentType()
     {
         return $this->contentType;

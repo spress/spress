f46ff2d05e7d1f2769dc8ca74cbcfff2cbe828ea
Yo! Symfony <yosymfony@gmail.com>
2016-03-07 19:10:37 +0000

    Added Collection in support classes

---
+++ src/Core/Support/Collection.php
@@ -0,0 +1,144 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\Core\Support;
+
+/**
+ * A colections of elements.
+ *
+ * @author Victor Puertas <vpgugr@gmail.com>
+ */
+class Collection implements \IteratorAggregate, \Countable
+{
+    protected $elements = [];
+
+    /**
+     * Constructor.
+     * 
+     * @param array $elements Elements.
+     */
+    public function __construct(array $elements = [])
+    {
+        $this->elements = $elements;
+    }
+
+    /**
+     * Returns an iterator over the elements.
+     *
+     * @return \ArrayIterator An \ArrayIterator object for iterating over elements.
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->elements);
+    }
+
+    /**
+     * Adds a new element in this collection.
+     *
+     * @param mixed $key     The key associated to the element.
+     * @param mixed $element The element.
+     */
+    public function add($key, $element)
+    {
+        if ($this->has($key) === false) {
+            $this->set($key, $element);
+        }
+    }
+
+    /**
+     * Gets a element from the collection.
+     *
+     * @param string $key The element identifier.
+     *
+     * @return mixed The element.
+     *
+     * @throws RuntimeException If the key is not defined.
+     */
+    public function get($key)
+    {
+        if ($this->has($key) === false) {
+            throw new \RuntimeException(sprintf('Element with key: "%s" not found.', $key));
+        }
+
+        return $this->elements[$key];
+    }
+
+    /**
+     * Gets the elements in this collection.
+     *
+     * @return array All elements in this collection.
+     */
+    public function all()
+    {
+        return $this->elements;
+    }
+
+    /**
+     * Gets the keys registered in this collection.
+     * 
+     * @return array
+     */
+    public function keys()
+    {
+        return array_keys($this->elements);
+    }
+
+    /**
+     * Checks if a elements exists in the collection.
+     *
+     * @param string $key The elements identifier or index.
+     *
+     * @return bool
+     */
+    public function has($key)
+    {
+        return array_key_exists($key, $this->elements);
+    }
+
+    /**
+     * Sets a element.
+     *
+     * @param mixed $key     The key associated to the element.
+     * @param mixed $element The element.
+     */
+    public function set($key, $element)
+    {
+        $this->elements[$key] = $element;
+    }
+
+    /**
+     * Gets the number of elements in this collection.
+     *
+     * @return int The number of elements.
+     */
+    public function count()
+    {
+        return count($this->elements);
+    }
+
+    /**
+     * Removes a element from the collection.
+     *
+     * @param string $key The element identifier or index.
+     */
+    public function remove($key)
+    {
+        unset($this->elements[$key]);
+    }
+
+    /**
+     * Clears all elements in this collection.
+     */
+    public function clear()
+    {
+        $this->elements = [];
+    }
+}
---
+++ src/Core/Tests/Support/CollectionTest.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\Core\Tests\Support;
+
+use Yosymfony\Spress\Core\Support\Collection;
+
+class CollectionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIterator()
+    {
+        $collection = new Collection();
+        $collection->add('acme', 'value');
+
+        $this->assertInstanceOf('\ArrayIterator', $collection->getIterator());
+        $this->assertSame([
+            'acme' => 'value',
+        ], $collection->getIterator()->getArrayCopy());
+    }
+
+    public function testAll()
+    {
+        $collection = new Collection();
+        $collection->add('acme', 'value');
+
+        $this->assertSame([
+            'acme' => 'value',
+        ], $collection->all());
+    }
+
+    public function testGet()
+    {
+        $collection = new Collection();
+        $collection->add('acme', 'value');
+
+        $this->assertSame('value', $collection->get('acme'));
+    }
+
+    public function testSet()
+    {
+        $collection = new Collection();
+        $collection->add('acme', 'value');
+        $collection->set('acme', 'value 2');
+
+        $this->assertSame('value 2', $collection->get('acme'));
+    }
+
+    public function testHas()
+    {
+        $collection = new Collection();
+        $collection->add('acme', 'value');
+
+        $this->assertTrue($collection->has('acme'));
+        $this->assertFalse($collection->has('acme 2'));
+    }
+
+    public function testKeys()
+    {
+        $collection = new Collection();
+        $collection->add('acme', 'value');
+        $collection->set('acme2', 'value 2');
+
+        $this->assertSame(['acme', 'acme2'], $collection->keys());
+    }
+
+    public function testCount()
+    {
+        $collection = new Collection();
+        $collection->add('acme', 'value');
+
+        $this->assertCount(1, $collection);
+    }
+
+    public function testRemove()
+    {
+        $collection = new Collection();
+        $collection->add('acme', 'value');
+        $collection->add('acme2', 'value 2');
+
+        $this->assertCount(2, $collection);
+
+        $collection->remove('acme2');
+
+        $this->assertCount(1, $collection);
+    }
+
+    public function testClear()
+    {
+        $collection = new Collection();
+        $collection->add('acme', 'value');
+        $collection->clear();
+
+        $this->assertCount(0, $collection);
+    }
+}

b106961d8de8cbc44eb850995797b6fcb14e5c34
yosymfony <yosymfony@gmail.com>
2016-09-18 21:03:25 +0000

    Added tests for Application class

---
+++ tests/Console/ApplicationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Yosymfony\Spress.
+ *
+ * (c) YoSymfony <http://github.com/yosymfony>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yosymfony\Spress\tests\Console;
+
+use Yosymfony\Spress\Console\Application;
+
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    protected $app;
+
+    public function setUp()
+    {
+        $autoloaders = spl_autoload_functions();
+
+        $this->app = new Application($autoloaders[0][0]);
+    }
+
+    public function testGetClassloader()
+    {
+        $this->assertInstanceOf('Composer\Autoload\ClassLoader', $this->app->getClassloader());
+    }
+
+    public function testGetSpress()
+    {
+        $this->assertInstanceOf('Yosymfony\Spress\Core\Spress', $this->app->getSpress());
+    }
+}

304e15058cf089928a456535eb56188f2d9b1b17
yosymfony <yosymfony@gmail.com>
2016-10-08 16:15:58 +0000

    CS fixes

--- src/Command/UpdatePluginCommand.php
+++ src/Command/UpdatePluginCommand.php
@@ -17,7 +17,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Yosymfony\Spress\PackageManager\PackageManager;
 
 /**
  * Update plugin command.

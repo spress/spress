55364cb4a0f3810b18def181ec08d80d702a274f
yosymfony <yosymfony@gmail.com>
2017-02-17 19:11:29 +0000

    Fixed some descriptions

--- src/Command/NewThemeCommand.php
+++ src/Command/NewThemeCommand.php
@@ -31,11 +31,11 @@ class NewThemeCommand extends BaseCommand
     protected function configure()
     {
         $this->setDefinition(array(
-            new InputArgument('path', InputArgument::OPTIONAL, 'Path of the new site', getcwd()),
-            new InputArgument('package', InputArgument::OPTIONAL, 'Packages that should be updated, if not provided all packages are.', ThemeGenerator::BLANK_THEME),
-            new InputOption('repository', null, InputOption::VALUE_REQUIRED, 'Pick a different repository (as url or json config) to look for the package.'),
-            new InputOption('prefer-source', null, InputOption::VALUE_NONE, 'Forces installation from package sources when possible, including VCS information.'),
-            new InputOption('dev', null, InputOption::VALUE_NONE, 'Enables installation of dev-require packages.'),
+            new InputArgument('path', InputArgument::OPTIONAL, 'Path of the new theme', getcwd()),
+            new InputArgument('package', InputArgument::OPTIONAL, 'The name of the package theme.', ThemeGenerator::BLANK_THEME),
+            new InputOption('repository', null, InputOption::VALUE_REQUIRED, 'Pick a different repository (as url or json config) to look for the package theme.'),
+            new InputOption('prefer-source', null, InputOption::VALUE_NONE, 'Forces installation of the theme from package sources when possible, including VCS information.'),
+            new InputOption('dev', null, InputOption::VALUE_NONE, 'Enables installation of dev-require packages of the theme.'),
             new InputOption('no-scripts', null, InputOption::VALUE_NONE, 'Skips the execution of all scripts defined in composer.json file.'),
             new InputOption('prefer-lock', '', InputOption::VALUE_NONE, 'If there is a "composer.lock" file in the theme, Spress will use the exact version declared in that'),
             new InputOption('force', '', InputOption::VALUE_NONE, 'Force creation even if path already exists'),
